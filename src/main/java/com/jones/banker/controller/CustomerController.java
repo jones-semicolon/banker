78692d8427db0e65012cae2fcc7fa518e06ad9c9
jones-semicolon <marjones.lapinig@gmail.com>
2025-10-01 13:04:21 +0800

    fix exceed amount

--- src/main/java/com/jones/banker/controller/CustomerController.java
+++ src/main/java/com/jones/banker/controller/CustomerController.java
@@ -23,7 +23,7 @@ public class CustomerController {
         Collections.reverse(reversed);
         model.addAttribute("customer", customer);
         model.addAttribute("transactions", reversed);
-        model.addAttribute("success", "transaction successful")
+        model.addAttribute("success", "transaction successful");
     }
 
     @GetMapping("/dashboard")
